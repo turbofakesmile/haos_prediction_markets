83cb528abc6ff8f7db5d1b822ec2edceb199adf8
Maksym Zub <kroistsmeh@gmail.com>
2024-12-22 16:45:45 +0100

    fix scripts

--- typescript/order-placer/gen-abi.sh
+++ typescript/order-placer/gen-abi.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 mkdir -p src/_abi
 echo "export const abi =" > src/_abi/orderbook.ts
-cat ../../out/OrderBook.sol/OrderBook.json | jq '.abi' >> src/_abi/orderbook.ts
+cat ../../contracts/out/OrderBook.sol/OrderBook.json | jq '.abi' >> src/_abi/orderbook.ts
 truncate -s -1 src/_abi/orderbook.ts
 echo -n "as const;" >> src/_abi/orderbook.ts
